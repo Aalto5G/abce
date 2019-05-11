3eea1f8ba516ebd93809a91754a403f1ba9b9a2f
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2019-05-11 22:10:39 +0300

    Initial commit

---
+++ memblock.c
@@ -0,0 +1,450 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "rbtree.h"
+#include "murmur.h"
+#include "containerof.h"
+
+struct abce {
+  void *(*alloc)(void *old, size_t newsz, void *alloc_baton);
+  void *alloc_baton;
+  void *userdata;
+};
+
+struct memblock;
+
+struct memblock_tree {
+  struct rb_tree_nocmp tree;
+};
+struct memblock_scope {
+  size_t size;
+  struct rb_tree_nocmp heads[0];
+};
+struct memblock_array {
+  struct memblock *mbs;
+  size_t capacity;
+  size_t size;
+};
+struct memblock_string {
+  size_t size;
+  char buf[0];
+};
+struct memblock_ios {
+  FILE *f;
+};
+struct memblockarea {
+  size_t refcnt;
+  union {
+    struct memblock_array ar;
+    struct memblock_ios ios;
+    struct memblock_scope sc;
+    struct memblock_tree tree;
+    struct memblock_string str;
+  } u;
+};
+enum type {
+  T_T,
+  T_D,
+  T_B,
+  T_F,
+  T_S,
+  T_PB, // packet buffer
+  T_IOS,
+  T_BP,
+  T_SP,
+  T_A,
+  T_SC,
+  T_N,
+};
+struct memblock {
+  enum type typ;
+  union {
+    double d;
+    struct memblockarea *area;
+  } u;
+};
+struct memblock_rb_entry {
+  struct rb_tree_node n;
+  struct memblock key; // must be a string!
+  struct memblock val;
+};
+
+void memblock_refdn(struct abce *abce, struct memblock *mb);
+
+static inline struct memblock
+memblock_refup(struct abce *abce, const struct memblock *mb)
+{
+  switch (mb->typ)
+  {
+    case T_T: case T_S: case T_IOS: case T_A: case T_SC:
+      mb->u.area->refcnt++;
+      break;
+    default:
+      break;
+  }
+  return *mb;
+}
+
+static inline struct memblock
+memblock_create_string(struct abce *abce, const char *str, size_t sz)
+{
+  struct memblockarea *mba;
+  struct memblock mb = {};
+  mba = abce->alloc(NULL, sizeof(*mba) + sz + 1, abce->alloc_baton);
+  memcpy(mba->u.str.buf, str, sz);
+  mba->u.str.buf[sz] = '\0';
+  mba->refcnt = 1;
+  mb.typ = T_S;
+  mb.u.area = mba;
+  return mb;
+}
+
+
+static inline uint32_t str_hash(char *str)
+{
+  size_t len = strlen(str);
+  return murmur_buf(0x12345678U, str, len);
+}
+static inline uint32_t mb_str_hash(const struct memblock *mb)
+{
+  if (mb->typ != T_S)
+  {
+    abort();
+  }
+  return murmur_buf(0x12345678U, mb->u.area->u.str.buf, mb->u.area->u.str.size);
+}
+static inline int str_cmp_asym(char *str, struct rb_tree_node *n2, void *ud)
+{
+  struct memblock_rb_entry *e = CONTAINER_OF(n2, struct memblock_rb_entry, n);
+  size_t len1 = strlen(str);
+  size_t len2, lenmin;
+  int ret;
+  char *str2;
+  if (e->key.typ != T_S)
+  {
+    abort();
+  }
+  len2 = e->key.u.area->u.str.size;
+  str2 = e->key.u.area->u.str.buf;
+  lenmin = (len1 < len2) ? len1 : len2;
+  ret = memcmp(str, str2, lenmin);
+  if (ret != 0)
+  {
+    return ret;
+  }
+  if (len1 > len2)
+  {
+    return 1;
+  }
+  if (len1 < len2)
+  {
+    return -1;
+  }
+  return 0;
+}
+
+static inline int str_cmp_halfsym(
+  const struct memblock *key, struct rb_tree_node *n2, void *ud)
+{
+  struct memblock_rb_entry *e2 = CONTAINER_OF(n2, struct memblock_rb_entry, n);
+  size_t len1, len2, lenmin;
+  int ret;
+  char *str1, *str2;
+  if (key->typ != T_S || e2->key.typ != T_S)
+  {
+    abort();
+  }
+  len1 = key->u.area->u.str.size;
+  str1 = key->u.area->u.str.buf;
+  len2 = e2->key.u.area->u.str.size;
+  str2 = e2->key.u.area->u.str.buf;
+  lenmin = (len1 < len2) ? len1 : len2;
+  ret = memcmp(str1, str2, lenmin);
+  if (ret != 0)
+  {
+    return ret;
+  }
+  if (len1 > len2)
+  {
+    return 1;
+  }
+  if (len1 < len2)
+  {
+    return -1;
+  }
+  return 0;
+}
+
+static inline int str_cmp_sym(
+  struct rb_tree_node *n1, struct rb_tree_node *n2, void *ud)
+{
+  struct memblock_rb_entry *e1 = CONTAINER_OF(n1, struct memblock_rb_entry, n);
+  struct memblock_rb_entry *e2 = CONTAINER_OF(n2, struct memblock_rb_entry, n);
+  size_t len1, len2, lenmin;
+  int ret;
+  char *str1, *str2;
+  if (e1->key.typ != T_S || e2->key.typ != T_S)
+  {
+    abort();
+  }
+  len1 = e1->key.u.area->u.str.size;
+  str1 = e2->key.u.area->u.str.buf;
+  len2 = e2->key.u.area->u.str.size;
+  str2 = e2->key.u.area->u.str.buf;
+  lenmin = (len1 < len2) ? len1 : len2;
+  ret = memcmp(str1, str2, lenmin);
+  if (ret != 0)
+  {
+    return ret;
+  }
+  if (len1 > len2)
+  {
+    return 1;
+  }
+  if (len1 < len2)
+  {
+    return -1;
+  }
+  return 0;
+}
+
+static inline const struct memblock *sc_get_val_mb(
+  const struct memblock *mb, const struct memblock *key)
+{
+  struct memblockarea *mba = mb->u.area;
+  uint32_t hashval;
+  size_t hashloc;
+  struct rb_tree_node *n;
+  if (mb->typ != T_SC)
+  {
+    abort();
+  }
+  if (key->typ != T_S)
+  {
+    abort();
+  }
+  hashval = mb_str_hash(key);
+  hashloc = hashval & (mba->u.sc.size - 1);
+  n = RB_TREE_NOCMP_FIND(&mba->u.sc.heads[hashloc], str_cmp_halfsym, NULL, key);
+  if (n == NULL)
+  {
+    return NULL;
+  }
+  return &CONTAINER_OF(n, struct memblock_rb_entry, n)->val;
+}
+
+static inline const struct memblock *sc_get_val_str(
+  const struct memblock *mb, char *str)
+{
+  struct memblockarea *mba = mb->u.area;
+  uint32_t hashval;
+  size_t hashloc;
+  struct rb_tree_node *n;
+  if (mb->typ != T_SC)
+  {
+    abort();
+  }
+  hashval = str_hash(str);
+  hashloc = hashval & (mba->u.sc.size - 1);
+  n = RB_TREE_NOCMP_FIND(&mba->u.sc.heads[hashloc], str_cmp_asym, NULL, str);
+  if (n == NULL)
+  {
+    return NULL;
+  }
+  return &CONTAINER_OF(n, struct memblock_rb_entry, n)->val;
+}
+
+static inline int sc_put_val_mb(
+  struct abce *abce,
+  const struct memblock *mb, const struct memblock *pkey, const struct memblock *pval)
+{
+  struct memblockarea *mba = mb->u.area;
+  uint32_t hashval;
+  struct memblock_rb_entry *e;
+  size_t hashloc;
+  int ret;
+  if (mb->typ != T_SC || pkey->typ != T_S)
+  {
+    abort();
+  }
+  hashval = mb_str_hash(pkey);
+  hashloc = hashval & (mba->u.sc.size - 1);
+  e = abce->alloc(NULL, sizeof(*e), abce->alloc_baton);
+  e->key = memblock_refup(abce, pkey);
+  e->val = memblock_refup(abce, pval);
+  ret = rb_tree_nocmp_insert_nonexist(&mba->u.sc.heads[hashloc],
+                                      str_cmp_sym, NULL, &e->n);
+  if (ret == 0)
+  {
+    return 0;
+  }
+  memblock_refdn(abce, &e->key);
+  memblock_refdn(abce, &e->val);
+  abce->alloc(e, 0, abce->alloc_baton);
+  return ret;
+}
+
+static inline int sc_put_val_str(
+  struct abce *abce,
+  const struct memblock *mb, char *str, const struct memblock *pval)
+{
+  struct memblockarea *mba = mb->u.area;
+  uint32_t hashval;
+  struct memblock_rb_entry *e;
+  size_t hashloc;
+  int ret;
+  if (mb->typ != T_SC)
+  {
+    abort();
+  }
+  hashval = str_hash(str);
+  hashloc = hashval & (mba->u.sc.size - 1);
+  e = abce->alloc(NULL, sizeof(*e), abce->alloc_baton);
+  e->key = memblock_create_string(abce, str, strlen(str));
+  e->val = memblock_refup(abce, pval);
+  ret = rb_tree_nocmp_insert_nonexist(&mba->u.sc.heads[hashloc],
+                                      str_cmp_sym, NULL, &e->n);
+  if (ret == 0)
+  {
+    return 0;
+  }
+  memblock_refdn(abce, &e->key);
+  memblock_refdn(abce, &e->val);
+  abce->alloc(e, 0, abce->alloc_baton);
+  return ret;
+}
+
+
+static inline size_t next_highest_power_of_2(size_t x)
+{
+  x--;
+  x |= x >> 1;
+  x |= x >> 2;
+  x |= x >> 4;
+  x |= x >> 8;
+  x |= x >> 16;
+#if SIZE_MAX > (4U*1024U*1024U*1024U)
+  x |= x >> 32;
+#endif
+  x++;
+  return x;
+}
+
+struct memblock memblock_create_scope(struct abce *abce, size_t capacity)
+{
+  struct memblockarea *mba;
+  struct memblock mb = {};
+  size_t i;
+
+  capacity = next_highest_power_of_2(capacity);
+  
+  mba = abce->alloc(NULL, sizeof(*mba) + capacity * sizeof(*mba->u.sc.heads),
+                    abce->alloc_baton);
+  mba->u.sc.size = capacity;
+  for (i = 0; i < capacity; i++)
+  {
+    rb_tree_nocmp_init(&mba->u.sc.heads[i]);
+  }
+  mba->refcnt = 1;
+  mb.typ = T_SC;
+  mb.u.area = mba;
+  return mb;
+}
+
+struct memblock memblock_create_tree(struct abce *abce)
+{
+  struct memblockarea *mba;
+  struct memblock mb = {};
+  mba = abce->alloc(NULL, sizeof(*mba), abce->alloc_baton);
+  rb_tree_nocmp_init(&mba->u.tree.tree);
+  mba->refcnt = 1;
+  mb.typ = T_A;
+  mb.u.area = mba;
+  return mb;
+}
+
+struct memblock memblock_create_array(struct abce *abce)
+{
+  struct memblockarea *mba;
+  struct memblock mb = {};
+  mba = abce->alloc(NULL, sizeof(*mba), abce->alloc_baton);
+  mba->u.ar.size = 0;
+  mba->u.ar.capacity = 16;
+  mba->u.ar.mbs =
+    abce->alloc(NULL, 16*sizeof(*mba->u.ar.mbs), abce->alloc_baton);
+  mba->refcnt = 1;
+  mb.typ = T_A;
+  mb.u.area = mba;
+  return mb;
+}
+
+void memblock_refdn(struct abce *abce, struct memblock *mb)
+{
+  size_t i;
+  switch (mb->typ)
+  {
+    case T_T:
+      if (!--mb->u.area->refcnt)
+      {
+        while (mb->u.area->u.tree.tree.root != NULL)
+        {
+          struct memblock_rb_entry *mbe =
+            CONTAINER_OF(mb->u.area->u.tree.tree.root,
+                         struct memblock_rb_entry, n);
+          memblock_refdn(abce, &mbe->key);
+          memblock_refdn(abce, &mbe->val);
+          rb_tree_nocmp_delete(&mb->u.area->u.tree.tree,
+                               mb->u.area->u.tree.tree.root);
+        }
+        abce->alloc(mb->u.area, 0, abce->alloc_baton);
+      }
+      break;
+    case T_IOS:
+      if (!--mb->u.area->refcnt)
+      {
+        fclose(mb->u.area->u.ios.f);
+        abce->alloc(mb->u.area, 0, abce->alloc_baton);
+      }
+      break;
+    case T_A:
+      if (!--mb->u.area->refcnt)
+      {
+        for (i = 0; i < mb->u.area->u.ar.size; i++)
+        {
+          memblock_refdn(abce, &mb->u.area->u.ar.mbs[i]);
+        }
+        abce->alloc(mb->u.area, 0, abce->alloc_baton);
+      }
+      break;
+    case T_S:
+      if (!--mb->u.area->refcnt)
+      {
+        abce->alloc(mb->u.area, 0, abce->alloc_baton);
+      }
+      break;
+    case T_SC:
+      for (i = 0; i < mb->u.area->u.sc.size; i++)
+      {
+        while (mb->u.area->u.sc.heads[i].root != NULL)
+        {
+          struct memblock_rb_entry *mbe =
+            CONTAINER_OF(mb->u.area->u.sc.heads[i].root,
+                         struct memblock_rb_entry, n);
+          memblock_refdn(abce, &mbe->key);
+          memblock_refdn(abce, &mbe->val);
+          rb_tree_nocmp_delete(&mb->u.area->u.sc.heads[i],
+                               mb->u.area->u.sc.heads[i].root);
+        }
+      }
+      break;
+    default:
+      break;
+  }
+  mb->typ = T_N;
+  mb->u.d = 0.0;
+  mb->u.area = NULL;
+}
+
+int main(int argc, char **argv)
+{
+}
