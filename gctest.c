7042429859a06b9ee9f6f6367ab747e166a9ce02
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2019-08-03 22:21:27 +0300

    rm unused variables

--- gctest.c
+++ gctest.c
@@ -13,8 +13,6 @@ int main(int argc, char **argv)
 {
   FILE *f = fmemopen(str, strlen(str), "r");
   struct amyplanyy amyplanyy = {};
-  unsigned char tmpbuf[1024] = {0};
-  size_t tmpsiz = 0;
   size_t i;
 
   amyplanyy_init(&amyplanyy);
