79a075c406d21b81cef4d5566b36387a9c9bf905
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2025-10-12 15:26:10 +0300

    Bootstrapping abce on environment without GNU make or stirmake

---
+++ bootstrapnomake.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+CC=cc
+CFLAGS="-O3 -Wall -g"
+
+die()
+{
+  echo "$@"
+  exit 1
+}
+
+libobjs=""
+
+for a in *.l; do
+  base="`echo "$a"|sed 's/.l$//g'`"
+  flex --outfile="$base.lex.c" --header-file="$base.lex.h" "$a" || die "flex"
+done
+for a in *.y; do
+  base="`echo "$a"|sed 's/.y$//g'`"
+  byacc -d -p "$base" -b "$base" -o "$base.tab.c" "$a" || die "byacc"
+done
+for a in *.c; do
+  base="`echo "$a"|sed 's/.c$//g'`"
+  if grep -q '^int main(int' "$a"; then
+    true
+  else
+    libobjs="$libobjs $base.o"
+  fi
+  $CC $CFLAGS -Wno-sign-compare -Wno-missing-prototypes -c -o "$base.o" "$a" || die "cc"
+done
+
+rm -f libabce.a
+ar rvs libabce.a $libobjs || die "ar"
