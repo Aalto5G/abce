cb395b173cb717a5ff84804b796bbffeb06c1b2d
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2026-02-19 18:40:55 +0200

    Do not use __builtin_expect except in clang and gcc

--- abcelikely.h
+++ abcelikely.h
@@ -1,5 +1,17 @@
 #ifndef _ABCE_LIKELY_H_
 #define _ABCE_LIKELY_H_
-#define abce_likely(x)       __builtin_expect((x),1)
-#define abce_unlikely(x)     __builtin_expect((x),0)
+
+#ifdef __clang__
+  #define abce_likely(x)       __builtin_expect(!!(x),1)
+  #define abce_unlikely(x)     __builtin_expect(!!(x),0)
+#else
+  #ifdef __GNUC__
+    #define abce_likely(x)       __builtin_expect(!!(x),1)
+    #define abce_unlikely(x)     __builtin_expect(!!(x),0)
+  #else
+    #define abce_likely(x)       (!!(x))
+    #define abce_unlikely(x)     (!!(x))
+  #endif
+#endif
+
 #endif
