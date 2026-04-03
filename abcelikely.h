0ddefa608ff2940ae6aba4f3bb77f1f1c2f0927e
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2026-04-03 14:54:41 +0300

    Another compilation fix

--- abcelikely.h
+++ abcelikely.h
@@ -5,6 +5,9 @@
   #if __has_builtin (__builtin_expect)
     #define abce_likely(x)       __builtin_expect(!!(x),1)
     #define abce_unlikely(x)     __builtin_expect(!!(x),0)
+  #else
+    #define abce_likely(x)       (!!(x))
+    #define abce_unlikely(x)     (!!(x))
   #endif
 #else
   #ifdef __clang__ // LLVM 3.0 is the first to use this, no-op on LLVM 1.0
