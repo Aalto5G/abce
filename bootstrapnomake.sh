93a5c096fcb56088ed4463527396776040458235
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2026-04-06 16:34:03 +0300

    Verbose build

--- bootstrapnomake.sh
+++ bootstrapnomake.sh
@@ -6,6 +6,13 @@ export CC="${CC:-cc}"
 export CFLAGS="${CFLAGS:--O3 -Wall -Wextra -Wno-unused-parameter -g}"
 export LDFLAGS="${LDFLAGS:-}"
 
+docmd()
+{
+  echo "$@"
+  "$@"
+  return $?
+}
+
 die()
 {
   echo "$@"
@@ -41,13 +48,13 @@ libobjs=""
 for a in *.l; do
   base="`echo "$a"|sed 's/.l$//g'`"
   if doflex "$base.lex.c" "$base.lex.h"; then
-    $FLEX --outfile="$base.lex.c" --header-file="$base.lex.h" "$a" || die "flex"
+    docmd $FLEX --outfile="$base.lex.c" --header-file="$base.lex.h" "$a" || die "flex"
   fi
 done
 for a in *.y; do
   base="`echo "$a"|sed 's/.y$//g'`"
   if dobyacc "$base.tab.c" "$base.tab.h"; then
-    $BYACC -d -p "$base" -b "$base" -o "$base.tab.c" "$a" || die "byacc"
+    docmd $BYACC -d -p "$base" -b "$base" -o "$base.tab.c" "$a" || die "byacc"
   fi
 done
 for a in *.c; do
@@ -57,8 +64,8 @@ for a in *.c; do
   else
     libobjs="$libobjs $base.o"
   fi
-  $CC $CFLAGS -Wno-sign-compare -Wno-missing-prototypes -c -o "$base.o" "$a" || die "cc"
+  docmd $CC $CFLAGS -Wno-sign-compare -Wno-missing-prototypes -c -o "$base.o" "$a" || die "cc"
 done
 
-rm -f libabce.a
-ar rvs libabce.a $libobjs || die "ar"
+docmd rm -f libabce.a
+docmd ar rvs libabce.a $libobjs || die "ar"
